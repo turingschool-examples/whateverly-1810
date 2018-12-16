4193a63ce343cff5bd9efd8d917d57642913f59f
Paul Vangelakos <paulvangelakos@gmail.com>
2018-12-16 16:49:35 -0700

    Adding Denver parks and landmarks dataset

---
+++ denver-parks-landmarks.js
@@ -0,0 +1,396 @@
+const cityParks = [
+  {
+  "parkName": "Bear Creek Park",
+  "parkType": "Regional",
+  "arces": 3,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1956,
+  "city": "Denver"
+},
+{
+  "parkName": "Douglass (Fredrick) Park",
+  "parkType": "Neigborhood",
+  "arces": 14,
+  "restrooms": false,
+  "picnicTables": true,
+  "yearDesignated": 1974,
+  "city": "Denver"
+},
+{
+  "parkName": "Confluence East Park",
+  "parkType": "Community",
+  "arces": 12,
+  "restrooms": false,
+  "picnicTables": true,
+  "yearDesignated": 2001,
+  "city": "Denver"
+},
+{
+  "parkName": "Community Plaza Park",
+  "parkType": "Pocket",
+  "arces": 14,
+  "restrooms": false,
+  "picnicTables": false,
+  "yearDesignated": 1908,
+  "city": "Denver"
+},
+{
+  "parkName": "Civic-Center Park",
+  "parkType": "Regional",
+  "arces": 12,
+  "restrooms": false,
+  "picnicTables": false,
+  "yearDesignated": 1911,
+  "city": "Denver"
+},
+{
+  "parkName": "Ashland Recreation Center",
+  "parkType": "Recreation Center",
+  "arces": 4,
+  "restrooms": true,
+  "picnicTables": false,
+  "yearDesignated": 1977,
+  "city": "Denver"
+},
+{
+  "parkName": "Denver Skate Park",
+  "parkType": "Special Use",
+  "arces": 14,
+  "restrooms": false,
+  "picnicTables": true,
+  "yearDesignated": 1993,
+  "city": "Denver"
+},
+{
+  "parkName": "Diamond Hill Promenade",
+  "parkType": "Pocket",
+  "arces": 17,
+  "restrooms": false,
+  "picnicTables": false,
+  "yearDesignated": 2008,
+  "city": "Denver"
+},
+{
+  "parkName": "Denison Park",
+  "parkType": "Neighborhood",
+  "arces": 6,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1977,
+  "city": "Denver"
+},
+{
+  "parkName": "Chavez (Cesar E) Park",
+  "parkType": "Neighborhood",
+  "arces": 4,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1977,
+  "city": "Denver"
+},
+{
+  "parkName": "Lower Lakewood Gulch",
+  "parkType": "Open Space",
+  "arces": 12,
+  "restrooms": false,
+  "picnicTables": false,
+  "yearDesignated": 2012,
+  "city": "Denver"
+},
+{
+  "parkName": "Cheesman Park",
+  "parkType": "Regional",
+  "arces": 19,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1890,
+  "city": "Denver"
+},
+{
+  "parkName": "Berkeley Lake Park",
+  "parkType": "Regional",
+  "arces": 4,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1906,
+  "city": "Denver"
+},
+{
+  "parkName": "Zeckendorf Plaza Park",
+  "parkType": "Pockey",
+  "arces": 8,
+  "restrooms": false,
+  "picnicTables": false,
+  "yearDesignated": 1966,
+  "city": "Denver"
+},
+{
+  "parkName": "West-Bar-Val-Wood Park",
+  "parkType": "Neighborhood",
+  "arces": 7,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1975,
+  "city": "Denver"
+},
+{
+  "parkName": "Centennial Gardens Park",
+  "parkType": "Special Use",
+  "arces": 127,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1976,
+  "city": "Denver"
+},
+{
+  "parkName": "Washington Park",
+  "parkType": "Regional",
+  "arces": 8,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1898,
+  "city": "Denver"
+},
+{
+  "parkName": "Valverde Park",
+  "parkType": "Athletic Complex",
+  "arces": 2,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1944,
+  "city": "Denver"
+},
+{
+  "parkName": "Wallace Park",
+  "parkType": "Neighborhood",
+  "arces": 6,
+  "restrooms": false,
+  "picnicTables": true,
+  "yearDesignated": 1988,
+  "city": "Denver"
+},
+{
+  "parkName": "Congress Park",
+  "parkType": "Community",
+  "arces": 198,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1890,
+  "city": "Denver"
+},
+{
+  "parkName": "Berkeley & Burlington Park",
+  "parkType": "Open Space",
+  "arces": 7,
+  "restrooms": false,
+  "picnicTables": false,
+  "yearDesignated": 1946,
+  "city": "Denver"
+},
+{
+  "parkName": "Quality Hill Park",
+  "parkType": "Pockey",
+  "arces": 6,
+  "restrooms": false,
+  "picnicTables": true,
+  "yearDesignated": 1948,
+  "city": "Denver"
+},
+{
+  "parkName": "Magna Carta Park",
+  "parkType": "Neighborhood",
+  "arces": 8,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1960,
+  "city": "Denver"
+},
+{
+  "parkName": "MacIntosh (Kenneth M) Park",
+  "parkType": "Pockey",
+  "arces": 4,
+  "restrooms": false,
+  "picnicTables": false,
+  "yearDesignated": 1967,
+  "city": "Denver"
+},
+{
+  "parkName": "Montclair Park",
+  "parkType": "Neighborhood",
+  "arces": 7,
+  "restrooms": false,
+  "picnicTables": true,
+  "yearDesignated": 1967,
+  "city": "Denver"
+},
+{
+  "parkName": "Southwest Auto Park",
+  "parkType": "Neighborhood",
+  "arces": 6,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1988,
+  "city": "Denver"
+},
+{
+  "parkName": "Governors Park",
+  "parkType": "Neighborhood",
+  "arces": 6,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1966,
+  "city": "Denver"
+},
+{
+  "parkName": "Genesse Park",
+  "parkType": "Mountain",
+  "arces": 52,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1982,
+  "city": "Denver"
+},
+{
+  "parkName": "Hallack Park",
+  "parkType": "Neighborhood",
+  "arces": 16,
+  "restrooms": false,
+  "picnicTables": true,
+  "yearDesignated": 1942,
+  "city": "Denver"
+},
+{
+  "parkName": "Rocky Mountain Lake Park",
+  "parkType": "Community",
+  "arces": 2,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1906,
+  "city": "Denver"
+},
+{
+  "parkName": "Fishback Park",
+  "parkType": "Neighborhood",
+  "arces": 86,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1976,
+  "city": "Denver"
+},
+{
+  "parkName": "Katherine Craig Park",
+  "parkType": "Mountain",
+  "arces": 61,
+  "restrooms": false,
+  "picnicTables": false,
+  "yearDesignated": 1975,
+  "city": "Denver"
+},
+{
+  "parkName": "Summit Lake Park",
+  "parkType": "Mountain",
+  "arces": 51,
+  "restrooms": true,
+  "picnicTables": true,
+  "yearDesignated": 1944,
+  "city": "Denver"
+}
+
+];
+
+
+
+const cityLandmarks = [
+
+{
+  "landmarkName": "Kerr House",
+  "historicDistrict": "Ballpark Neighborhood",
+  "yearDesignated": 1976,
+  "address": "1900 E. 7th Avenue Parkway",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Ballpark Neighborhood",
+  "historicDistrict": "Ballpark Neighborhood",
+  "yearDesignated": 1976,
+  "address": "770 Pennsylvania Street",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Equitable Building",
+  "historicDistrict": "Civic Center",
+  "yearDesignated": 1977,
+  "address": "730 17th Street",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Benjamin Brown House ",
+  "historicDistrict": "Clements",
+  "yearDesignated": 1997,
+  "address": "410 Marion Street",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Crescent Hand Laundry Building",
+  "historicDistrict": "Downtown Denver",
+  "yearDesignated": 1996,
+  "address": "2323-2327 W. 30th Avenue",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Sacred Heart Church",
+  "historicDistrict": "Downtown Denver",
+  "yearDesignated": 1973,
+  "address": "2760 Larimer Street",
+  "city": "Denver"
+},
+{
+  "landmarkName": "US Post Office and Federal Building",
+  "historicDistrict": "Downtown Denver",
+  "yearDesignated": 1974,
+  "address": "1823 Stout Street",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Four Mile House",
+  "historicDistrict": "Larimer Square",
+  "yearDesignated": 1968,
+  "address": "715 S. Forest Street",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Nagel House",
+  "historicDistrict": "Five Points Historic Cultural District",
+  "yearDesignated": 1998,
+  "address": "2335 Stout Street",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Schulz-Neef House",
+  "historicDistrict": "Lower Downtown",
+  "yearDesignated": 2007,
+  "address": "1739 E. 29th Avenue",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Gate Lodge at Fairmount Cemetery",
+  "historicDistrict": "East 7th Avenue",
+  "yearDesignated": 1976,
+  "address": "400 S. Quebec Street",
+  "city": "Denver"
+},
+{
+  "landmarkName": "Tramway Cable Building",
+  "historicDistrict": "Swallow Hill",
+  "yearDesignated": 1972,
+  "address": "1801 Lawrence Street",
+  "city": "Denver"
+}
+
+];
+
+module.exports = {
+  cityParks,
+  cityLandmarks
+};
