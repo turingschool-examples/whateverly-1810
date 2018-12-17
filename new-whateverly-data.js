8ffa4e185ac2142bd8c801d880a06f146045df9c
Libby Yeh <33442154+libbyeh@users.noreply.github.com>
2018-12-17 08:31:30 -0700

    Libby Dataset - Denver Restaurants

---
+++ new-whateverly-data.js
@@ -0,0 +1,470 @@
+bestRestaurants {
+  "city": "Denver",
+  "population": 704,621,
+  "foodCity": "Trying to be"
+  "bestRestaurants": [
+    {
+      "name": "OneFold",
+      "type": "Asian",
+      "neighborhood": "Uptown",
+      "stars": 5,
+      "reviews": 563,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-12am",
+        "Sunday Brunch 10am-2pm",
+        "Sunday Dinner 4pm-10pm"
+      ]
+    },
+    {
+      "name": "Cafe Brazil",
+      "type": "Brazilian",
+      "neighborhood": "Sunnyside",
+      "stars": 4,
+      "reviews": 821,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-12am",
+        "Sunday 4pm-10pm"
+      ] 
+    },
+    {
+      "name": "Lao Wong",
+      "type": "Taiwanese",
+      "neighborhood": "Federal",
+      "stars": 5,
+      "reviews": 1209,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 5pm-8pm",
+        "Friday/Saturday 5pm-9am",
+        "Sunday 4pm-7pm"
+      ]
+    },
+    {
+      "name": "Cart Driver"
+      "type": "Pizza",
+      "neighborhood": "RINO",
+      "stars": 5,
+      "reviews": 468,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 11am-10pm",
+        "Friday/Saturday 11am-12am",
+        "Sunday 10am-10pm"
+      ]
+    },
+    {
+      "name": "Cuba Cuba",
+      "type": "Cuban",
+      "neighborhood": "Downtown",
+      "stars": 5,
+      "reviews": 902,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-12am",
+        "Sunday Brunch 10am-2pm",
+        "Sunday Dinner 4pm-10pm"
+      ]
+    },
+    {
+      "name": "Tortas ATM",
+      "type": "Mexican",
+      "neighborhood": "Federal",
+      "stars": 3,
+      "reviews": 291,
+      "alcoholServed": "false",
+      "hours": [
+        "Weekdays (Mon-Thurs) 10am-10pm",
+        "Friday/Saturday 4pm-12am",
+        "Sunday 11am-10pm"
+      ] 
+    },
+    {
+      "name": "New Saigon",
+      "type": "Vietnamese",
+      "neighborhood": "Federal",
+      "stars": 5,
+      "reviews": 1927,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 12pm-10pm",
+        "Friday/Saturday 12pm-11pm",
+        "Sunday 11am-10pm"
+      ]
+    },
+    {
+      "name": "Star Kitchen"
+      "type": "Chinese",
+      "neighborhood": "Federal",
+      "stars": 5,
+      "reviews": 1378,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 11am-10pm",
+        "Friday/Saturday 11am-11pm",
+        "Sunday 9am-10pm"
+      ]
+    },
+    {
+      "name": "Ethiopia",
+      "type": "Ethiopian",
+      "neighborhood": "Colfax",
+      "stars": 4,
+      "reviews": 840,
+      "alcoholServed": "false",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-12am",
+        "Sunday 4pm-10pm"
+      ]
+    },
+    {
+      "name": "Yak & Yeti",
+      "type": "Indian",
+      "neighborhood": "Arvada",
+      "stars": 4,
+      "reviews": 659,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 12pm-10pm",
+        "Friday/Saturday 12pm-11pm",
+        "Sunday 11am-10pm"
+      ] 
+    },
+    {
+      "name": "Potager",
+      "type": "American",
+      "neighborhood": "Uptown",
+      "stars": 5,
+      "reviews": 909,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 5pm-10pm",
+        "Friday/Saturday 5pm-11pm",
+        "Sunday Brunch 10am-2pm",
+        "Sunday 4pm-7pm"
+      ]
+    },
+    {
+      "name": "The Spice Room"
+      "type": "Indian",
+      "neighborhood": "Federal",
+      "stars": 4,
+      "reviews": 138,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-10pm",
+        "Sunday 10am-10pm"
+      ]
+    },
+    {
+      "name": "Aki Hotpot",
+      "type": "Chinese",
+      "neighborhood": "Colorado",
+      "stars": 4,
+      "reviews": 4802,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-12am",
+        "Sunday 4pm-10pm"
+      ]
+    },
+    {
+      "name": "White Pie",
+      "type": "Pizza",
+      "neighborhood": "City PArk",
+      "stars": 4,
+      "reviews": 589,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 12pm-10pm",
+        "Friday/Saturday 12pm-11pm",
+        "Sunday 11am-10pm"
+      ] 
+    },
+    {
+      "name": "Tomayo",
+      "type": "Mexian",
+      "neighborhood": "Larimer Square",
+      "stars": 5,
+      "reviews": 719,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-11pm",
+        "Sunday Brunch 10am-2pm",
+        "Sunday 4pm-10pm"
+      ]
+    },
+    {
+      "name": "Work & Class"
+      "type": "American",
+      "neighborhood": "RINO",
+      "stars": 5,
+      "reviews": 638,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-10pm",
+        "Sunday 10am-10pm"
+      ]
+    },
+    {
+      "name": "Beast & Bottle",
+      "type": "American",
+      "neighborhood": "Uptown",
+      "stars": 4,
+      "reviews": 548,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 5pm-10pm",
+        "Friday/Saturday 5pm-11pm",
+        "Sunday 5pm-10pm"
+      ]
+    },
+    {
+      "name": "Frasca",
+      "type": "Italian",
+      "neighborhood": "Boulder",
+      "stars": 4,
+      "reviews": 732,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-11pm",
+        "Sunday 11am-10pm"
+      ] 
+    },
+    {
+      "name": "Potager",
+      "type": "American",
+      "neighborhood": "Uptown",
+      "stars": 5,
+      "reviews": 909,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 5pm-10pm",
+        "Friday/Saturday 5pm-11pm",
+        "Sunday Brunch 10am-2pm",
+        "Sunday 4pm-7pm"
+      ]
+    },
+    {
+      "name": "Bar Dough"
+      "type": "Pizza",
+      "neighborhood": "Highlands",
+      "stars": 5,
+      "reviews": 793,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 11am-10pm",
+        "Friday/Saturday 11am-10pm",
+        "Sunday 11am-10pm"
+      ]
+    },
+    {
+      "name": "Uncle",
+      "type": "Ramen",
+      "neighborhood": "Highlands",
+      "stars": 5,
+      "reviews": 448,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-11pm",
+        "Sunday 4pm-10pm"
+      ]
+    },
+    {
+      "name": "Sushi Ronin",
+      "type": "Japanese",
+      "neighborhood": "Lohi",
+      "stars": 4,
+      "reviews": 539,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-11pm",
+        "Sunday 11am-10pm"
+      ] 
+    },
+    {
+      "name": "El Five",
+      "type": "Mediterranean",
+      "neighborhood": "Lohi",
+      "stars": 4,
+      "reviews": 309,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 5pm-10pm",
+        "Friday/Saturday 5pm-11pm",
+        "Sunday Brunch 10am-2pm",
+        "Sunday 4pm-10pm"
+      ]
+    },
+    {
+      "name": "Senor Bear"
+      "type": "Latin",
+      "neighborhood": "Lohi",
+      "stars": 4,
+      "reviews": 432,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-11pm",
+        "Sunday Brunch 10am-2pm",
+        "Sunday 4pm-10pm"
+      ]
+    },
+    {
+      "name": "ChoLon Bistro",
+      "type": "Chinese",
+      "neighborhood": "Downtown",
+      "stars": 5,
+      "reviews": 835,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-11pm",
+        "Sunday 4pm-10pm"
+      ]
+    },
+    {
+      "name": "Mercantile Dining & Provisions",
+      "type": "American",
+      "neighborhood": "Union Station",
+      "stars": 4,
+      "reviews": 468,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-11pm",
+        "Sunday 11am-10pm"
+      ] 
+    },
+    {
+      "name": "El Taco de Mexico",
+      "type": "Mexican",
+      "neighborhood": "Lohi",
+      "stars": 5,
+      "reviews": 279,
+      "alcoholServed": "false",
+      "hours": [
+        "Weekdays (Mon-Thurs) 11am-10pm",
+        "Friday/Saturday 11am-10pm",
+        "Sunday 11am-10pm"
+      ]
+    },
+    {
+      "name": "Bijus Curry House"
+      "type": "Indian",
+      "neighborhood": "Lohi",
+      "stars": 4,
+      "reviews": 199,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 11am-9pm",
+        "Friday/Saturday 11am-10pm",
+        "Sunday 11am-9pm"
+      ]
+    },
+    {
+      "name": "Root Down",
+      "type": "American",
+      "neighborhood": "Lohi",
+      "stars": 5,
+      "reviews": 897,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 5pm-10pm",
+        "Friday/Saturday 5pm-11pm",
+        "Sunday Brunch 10am-2pm",
+        "Sunday 4pm-10pm"
+      ]
+    },
+    {
+      "name": "City O City"
+      "type": "Vegan",
+      "neighborhood": "Uptown",
+      "stars": 4,
+      "reviews": 739,
+      "alcoholServed": "true",
+      "hours": [
+        "Weekdays (Mon-Thurs) 4pm-10pm",
+        "Friday/Saturday 4pm-11pm",
+        "Sunday Brunch 10am-2pm",
+        "Sunday 4pm-10pm"
+      ]
+    },
+  ]
+}
+
+
+newRestaurants [
+    {
+      "name": "Beckon/Call",
+      "type": "American",
+      "neighborhood": "Larimer Square"
+    },
+    {
+      "name": "Chow Morso Osteria",
+      "type": "Italian",
+      "neighborhood": "Union Station"
+    },
+    {
+      "name": "Corrida",
+      "type": "Spanish",
+      "neighborhood": "RINO"
+    },
+    {
+      "name": "Julip",
+      "type": "Southern",
+      "neighborhood": "RINO"
+    },
+    {
+      "name": "Morin",
+      "type": "Parisian",
+      "neighborhood": "Downtown"
+    },
+    {
+      "name": "Q House",
+      "type": "Chinese",
+      "neighborhood": "Colfax"
+    },
+    {
+      "name": "Safta",
+      "type": "Israeli",
+      "neighborhood": "RINO"
+    },
+    {
+      "name": "Smok",
+      "type": "BBQ",
+      "neighborhood": "RINO"
+    },
+        {
+      "name": "Super Mega Bien",
+      "type": "Latin",
+      "neighborhood": "RINO"
+    },
+    {
+      "name": "Uchi",
+      "type": "Japanese",
+      "neighborhood": "RINO"
+    },
+    {
+      "name": "The Wolfs Tailor",
+      "type": "Asian Fusion",
+      "neighborhood": "Higlands"
+    }
+]
+
+module.exports = {
+  bestRestaurants,
+  newRestaurants
+}
