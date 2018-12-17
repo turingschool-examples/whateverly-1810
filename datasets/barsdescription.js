da36c48df99c7dc00d742ec6ec55010bacce1dcc
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2018-12-17 08:43:51 -0700

    initial commit

---
+++ datasets/barsdescription.js
@@ -0,0 +1,124 @@
+{
+  "bars": [
+    "Union Lodge No.1": {
+      "description": "Slinky speakeasy serving 19th-century-inspired cocktails in glam, Prohibition-era environs.",
+      "rating": 4.7,
+      "food": false,
+      "site": "http://www.unionlodge1.com/"
+    },
+    "Viewhouse Ballpark": {
+      "description": "Popular bar & grill with a scenic outdoor patio serving locavore comfort grub, craft beers & wines.",
+      "rating": 4.5,
+      "food": true,
+      "site": "http://www.viewhouse.com/location-ballpark/"
+    },
+    "Star Bar": {
+      "description": "Enduring pub (since 1959) featuring a robust beer & spirits lineup, plus a back patio & Skee-Ball.",
+      "rating": 4.2,
+      "food": false,
+      "site": "https://www.starbardenver.com/"
+    },
+    "Terminal Bar": {
+      "description": "Enduring pub (since 1959) featuring a robust beer & spirits lineup, plus a back patio & Skee-Ball.",
+      "rating": 4.2,
+      "food": true,
+      "site": "https://www.terminalbardenver.com/location/"
+    },
+    "Thu 1up": {
+      "description": "Retro arcade & pinball games in a bustling, no-frills basement bar with pub food & a jukebox.",
+      "rating": 4.4,
+      "food": true,
+      "site": "http://www.the-1up.com/",
+    },
+    "Triangle Denver": {
+      "description": "Happy hour food - Happy hour drinks - dancing"
+      "rating": 4.3,
+      "food": true,
+      "site": "http://www.triangledenver.com/"
+    },
+    "The British Bulldog": {
+      "description": "Dark, woody pub with British/Indian eats, drink specials & early hours for weekend soccer from U.K.",
+      "rating": 4.5,
+      "food": true,
+      "site": "https://britishbulldogdenver.com/"
+    },
+    "El Chapultepec": {
+      "description": "Well-known nightclub with regular live jazz, late hours & Mexican chow served on paper plates.",
+      "rating": 4.3,
+      "food": true,
+      "site": "http://www.thepeclodo.com/"
+    },
+    "CRU Food & Wine Bar": {
+      "description": "Relaxed, Napa-style wine-bar chain serving New American fare, stone-fired pizzas & cheese plates.",
+      "rating": 4.5,
+      "food": true,
+      "site": "https://cruawinebar.com/"
+    },
+    "Lodo\"s Bar & Grill": {
+      "description": "Lively hangout showing live sports on TV, serving burgers, sandwiches & weekend brunch.",
+      "rating": 3.8,
+      "food": true,
+      "site": "https://lodosbarandgrill.c"
+    },
+    "Icehouse Tavern": {
+      "description": "Saloon with big oval bar, flat-screens & pool tables that lures locals before & after Rockies games.",
+      "rating": 4.1,
+      "food": true,
+      "site": "https://icehouselodo.com/"
+    },
+    "Wiskey Bar": {
+      "description": "Comfy Coors Field-area bar with an array of whiskey (& non-whiskey) choices plus foosball & pool.",
+      "rating": 4.4,
+      "food": false,
+      "site": "http://www.whiskeybardenver.com/"
+    },
+    "Beta Nightclub": {
+      "description": "Famed DJs spin dubstep, techno & dance music that benefits from a quality sound system.",
+      "rating": 3.8,
+      "food": false,
+      "site": "https://www.betanightclub.com/"
+    },
+    "The Cruise Room": {
+      "description": "Historic, upscale Art Deco martini bar in the Oxford Hotel modeled after a lounge on the Queen Mary.",
+      "rating": 4.4,
+      "food": true,
+      "site": "https://www.theoxfordhotel.com/denver-dining/the-cruise-room/"
+    },
+    "Green Russell": {
+      "description": "Stylish underground lair devoted to custom libations & small plates from chef-partner Frank Bonnano.",
+      "rating": 4.0,
+      "food": true,
+      "site": "https://www.greenrussell.com/"
+    },
+    "Poka Lola Social Club": {
+      "description": "Old-fashioned soda fountains inspire this sophisticated cocktail lounge in a public lobby.",
+      "rating": 4.5,
+      "food": false,
+      "site": "https://pokaloladenver.com/"
+    },
+    "Tavern Downtown": {
+      "description": "A huge multilevel sports bar that has TVs galore, including on the sweeping rooftop with city views.",
+      "rating": 3.9,
+      "food": true,
+      "site": "http://www.tavernhg.com/downtown"
+    },
+    "Rock Bottom Restaurant & Brewery": {
+      "description": "Brewpub chain serving house beers & upscale pub food & American fare in lively environs.",
+      "rating": 3.9,
+      "food": true,
+      "site": "https://rockbottom.com/locations/denver/?utm_source=local&utm_medium=listings&utm_campaign=imm"
+    },
+    "Ophelia\" Electric Soapbox": {
+      "description": "Huge, multilevel \"gastro-brothel\" with live music & a restaurant offering eclectic American food.",
+      "rating": 4.6,
+      "food": true,
+      "site": "https://www.opheliasdenver.com/"
+    },
+    "Brothers Bar & Grill": {
+      "description": "Outpost of a Midwestern sports-bar chain serving casual American eats in a party atmosphere.",
+      "rating": 3.8,
+      "food": true,
+      "site": "https://www.brothersbar.com/"
+    }
+  ]
+}
---
+++ datasets/barsnearturing.js
@@ -0,0 +1,104 @@
+{
+  "bars": [
+  {
+    "Name": "Union Lodge No.1",
+    "location": "1543 Champa St, Denver, CO 80202",
+    "distance": "0.4 mile"
+  },
+  {
+    "Name": "ViewHouse BallPark",
+    "location": "2015 Market St, Denver, CO 80205",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Star Bar",
+    "location": "2137 Larimer St, Denver, CO 80205",
+    "distance": "0.5 mile"
+  },
+  {
+    "Name": "Terminal Bar",
+    "location": "1701 Wynkoop St, Denver, CO 80211",
+    "distance": "0.2 mile"
+  },
+  {
+    "Name": "The 1up",
+    "location": "1925 Blake St, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Triangle Denver",
+    "location": "2036 N Broadway, Denver, CO 80205",
+    "distance": "0.8 mile"
+  },
+  {
+    "Name": "The British Bulldog",
+    "location": "2052 Stout St, Denver, CO 80205",
+    "distance": "0.7 mile"
+  },
+  {
+    "Name": "El Chapultepec",
+    "location": "1962 Market St, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "CRU Food & Wine Bar",
+    "location": "1442 Larimer St, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Lodo\'s Bar & Grill",
+    "location": "1946 Market St, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Icehouse Tavern",
+    "location": "1801 Wynkoop St # 150, Denver, CO 80202",
+    "distance": "0.4 mile"
+  },
+  {
+    "Name": "Wiskey Bar",
+    "location": "2203 Larimer St, Denver, CO 80205",
+    "distance": "0.5 mile"
+  },
+  {
+    "Name": "Beta Nightclub",
+    "location": "1909 Blake St, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "The Cruise Room",
+    "location": "1600 17th St, Denver, CO 80202",
+    "distance": "0.2 mile"
+  },
+  {
+    "Name": "Green Russell",
+    "location": "1422 Larimer St, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Poka Lola Social Club",
+    "location": "1850 Wazee St., Ground Floor, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Tavern Downtown",
+    "location": "1949 Market St, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Rock Bottom Restaurant & Brewery",
+    "location": "1001 16th St Mall #A-100, Denver, CO 80265",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Ophelia\'s Electric Soapbox",
+    "location": "1215 20th St, Denver, CO 80202",
+    "distance": "0.3 mile"
+  },
+  {
+    "Name": "Brothers Bar & Grill",
+    "location": "1920 Market St, Denver, CO 80202",
+    "distance": "0.2 mile"
+  }
+  ]
+}
