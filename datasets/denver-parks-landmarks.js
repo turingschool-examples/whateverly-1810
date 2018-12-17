67ccf78bf39aaee9d40f06fcccff549103615840
Paul Vangelakos <paulvangelakos@gmail.com>
2018-12-16 17:23:32 -0700

    Adding more information to the landmarks data

--- datasets/denver-parks-landmarks.js
+++ datasets/denver-parks-landmarks.js
@@ -308,86 +308,97 @@ const cityLandmarks = [
   "historicDistrict": "Ballpark Neighborhood",
   "yearDesignated": 1976,
   "address": "1900 E. 7th Avenue Parkway",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "The John G. and Helen Kerr House, built in 1924-1925, is a good example of the Italian Renaissance Revival style that was popular during the early 20th century.  Located on a large corner lot, the two-story brick residence reflects the extensive and artistic use of Colorado travertine marble. "
 },
 {
   "landmarkName": "Ballpark Neighborhood",
   "historicDistrict": "Ballpark Neighborhood",
   "yearDesignated": 1976,
   "address": "770 Pennsylvania Street",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "During its peak, this area revolved around Denver’s rail line with warehouses and factories all catering to train-related activities. As this part of Downtown grew alongside the railroad tracks that carried goods and raw materials into Denver’s industrial market, waves of immigrants – Italian, Irish, Japanese, Chinese and, more recently, Mexican – left their own unique stamp on the area."
 },
 {
   "landmarkName": "Equitable Building",
   "historicDistrict": "Civic Center",
   "yearDesignated": 1977,
   "address": "730 17th Street",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "Standing at a height of 125 feet with 9 stories, the steel-framed office tower became the tallest building in the city upon its completion in 1892, and retained that title until 1911, when it was surpassed by the Daniels & Fisher Tower.[2] It is an excellent example of the Italian Renaissance Revival architecture of its day, with its numerous sets of arched windows and the ornately carved granite that define its appearance."
 },
 {
-  "landmarkName": "Benjamin Brown House ",
-  "historicDistrict": "Clements",
-  "yearDesignated": 1997,
-  "address": "410 Marion Street",
-  "city": "Denver"
+  "landmarkName": "Brown Palace Hotel",
+  "historicDistrict": "Downtown Denver",
+  "yearDesignated": 1970,
+  "address": "321 17th Street ",
+  "city": "Denver",
+  "description": "The Brown Palace Hotel and Spa, Autograph Collection is a historic hotel in Denver, Colorado, United States. It is the second-longest operating hotel in Denver and is one of the first atrium-style hotels ever built."
 },
 {
   "landmarkName": "Crescent Hand Laundry Building",
   "historicDistrict": "Downtown Denver",
   "yearDesignated": 1996,
   "address": "2323-2327 W. 30th Avenue",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "One of the oldest buildings in Denver"
 },
 {
   "landmarkName": "Sacred Heart Church",
   "historicDistrict": "Downtown Denver",
   "yearDesignated": 1973,
   "address": "2760 Larimer Street",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "Sacred Heart is Denver's oldest still-used church. St. Mary's, the pioneer 1860 church, was demolished in 1900, and St. Elizabeth's--the second Denver parish--outgrew its original 1879 church, which was torn down in 1898 to build the current St. Elizabeth's."
 },
 {
   "landmarkName": "US Post Office and Federal Building",
   "historicDistrict": "Downtown Denver",
   "yearDesignated": 1974,
   "address": "1823 Stout Street",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "Now known as the Byron White U.S. Courthouse, this building brought design elements popular in the eastern United States to Denver. The monumental scale and elegance expressed its official and public character, and served as inspiration for other civic buildings in the city."
 },
 {
   "landmarkName": "Four Mile House",
   "historicDistrict": "Larimer Square",
   "yearDesignated": 1968,
   "address": "715 S. Forest Street",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "Named for its location four miles from the intersection of Broadway and Colfax Avenue in Denver, Four Mile House was built in 1859 and served in the 1860s as the last stage stop before the city along the Smoky Hill Trail.When railroads replaced stagecoaches in the 1870s, the property became a farm operated for many decades by members of the Booth and Working families.Preserved since the 1970s as part of Four Mile Historic Park, the house is now the oldest residential building in the Denver area."
 },
 {
-  "landmarkName": "Nagel House",
-  "historicDistrict": "Five Points Historic Cultural District",
-  "yearDesignated": 1998,
-  "address": "2335 Stout Street",
-  "city": "Denver"
+  "landmarkName": "Denver Botantic Gardens",
+  "historicDistrict": "Cheesman Park",
+  "yearDesignated": 1951,
+  "address": "1007 York St",
+  "city": "Denver",
+  "description": "One of the top five botanic gardens in the nation, Denver Botanic Gardens is an oasis in the city, offering year-round events, Mordecai Children’s Garden, lifelong learning opportunities and research to preserve Colorado’s precious natural resources."
 },
 {
   "landmarkName": "Schulz-Neef House",
   "historicDistrict": "Lower Downtown",
   "yearDesignated": 2007,
   "address": "1739 E. 29th Avenue",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "The Schulz-Neef house is one of the oldest homes in the Whittier Neighborhood. Its steeply-gabled roof, ornamental woodwork and pointed finials stand out as a fine example of the Gothic Revival architectural style, a rarity in Denver residences."
 },
 {
   "landmarkName": "Gate Lodge at Fairmount Cemetery",
   "historicDistrict": "East 7th Avenue",
   "yearDesignated": 1976,
   "address": "400 S. Quebec Street",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "Founded 1890 by the Fairmount Cemetery Association with over 800 acres and water from the Highline Canal, Fairmount Cemetery was the largest developed landscape at the time, west of the Mississippi. Designed by renowned landscape architect Reinhard Schuetze, father of Denver’s city park system, Fairmount is Colorado‘s largest arboretum; home to numerous Champion Trees and one of the largest collections of Heritage Roses in North America."
 },
 {
   "landmarkName": "Tramway Cable Building",
   "historicDistrict": "Swallow Hill",
   "yearDesignated": 1972,
   "address": "1801 Lawrence Street",
-  "city": "Denver"
+  "city": "Denver",
+  "description": "The Denver Tramway, operating in Denver, Colorado, was a streetcar system incorporated in 1886. The tramway was unusual for a number of reasons: the term tramway is generally not used in the United States, and it is not known why the company was named as such."
 }
-
 ];
 
 module.exports = {
