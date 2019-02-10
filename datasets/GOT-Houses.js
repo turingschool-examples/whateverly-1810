02d89dcff9bba638b39b16083b7afe7e663b5d2d
Rachael Drennan <rdrennan0@gmail.com>
2019-02-10 16:43:07 -0700

    add data sets

---
+++ datasets/GOT-Houses.js
@@ -0,0 +1,201 @@
+const Houses = [
+{"name": "House Targaryen of King's Landing",
+"greatHouse": true,
+"region": "The Crownlands",
+"coatOfArms": "Sable, a dragon thrice-headed gules",
+"words": "Fire and Blood",
+"titles": [
+    "King of the Andals, the Rhoynar and the First Men",
+    "Lord of the Seven Kingdoms",
+    "Prince of Summerhall"
+  ]
+},
+{"name": "House Stark of Winterfell",
+"greatHouse": true,
+"region":"The North",
+"coatOfArms": "A running grey direwolf, on an ice-white field",
+"words": "Winter is Coming",
+"titles": ["King in the North",
+    "Lord of Winterfell",
+    "Warden of the North",
+    "King of the Trident"] 
+},
+{"name": "House Lannister of Casterly Rock",
+"greatHouse": true,
+"region": "The Westerlands",
+"coatOfArms": "A gold lion, on a crimson field(Gules, a lion or)",
+"words": "Hear Me Roar!",
+"titles": [
+    "King of the Rock (formerly) Lord of Casterly Rock",
+    "Shield of Lannisport",
+    "Warden of the West"
+  ] 
+},
+{"name": "House Arryn of the Eyrie",
+"greatHouse": true,
+"region": "The Vale",
+"coatOfArms": "A sky-blue falcon soaring against a white moon, on a sky-blue field(Bleu celeste, upon a plate a falcon volant of the field)",
+"words": "As High as Honor",
+"titles": [
+    "King of Mountain and Vale (formerly)",
+    "Lord of the Eyrie",
+    "Defender of the Vale",
+    "Warden of the East"
+  ] 
+},
+{"name": "House Tully of Riverrun",
+"greatHouse": true,
+"region": "The Riverlands",
+"coatOfArms": "A leaping silver trout on a field of blue and mud red(Paly wavy azure and gules, a trout embowed argent)",
+"words": "Family, Duty, Honor",
+"titles": [
+    "Lord of Riverrun",
+    "Lord Paramount of the Trident"
+  ] 
+},
+{"name": "House Greyjoy of Pyke",
+"greatHouse": true,
+"region": "Iron Islands",
+"coatOfArms": "Sable, a kraken Or",
+"words": "We Do Not Sow",
+"titles": [
+    "King of Salt and Rock",
+    "Son of the Sea Wind",
+    "Lord Reaper of Pyke"
+  ] 
+},
+{"name": "House Baratheon of Storm's End",
+"greatHouse": true,
+"region": "The Stormlands",
+"coatOfArms": "A black crowned stag, on a gold field(Or, a stag salient, crowned, sable)",
+"words": "Ours is the Fury",
+"titles": [
+    "Lord of Storm's End",
+    "Lord Paramount of the Stormlands"
+  ] 
+},
+{"name": "House Tyrell of Highgarden",
+"greatHouse": true,
+"region": "The Reach",
+"coatOfArms": "Vert, a rose Or",
+"words": "Growing Strong",
+"titles":  [
+    "Lord of Highgarden",
+    "Defender of the Marches",
+    "High Marshal of the Reach",
+    "Warden of the South",
+    "Lord Paramount of the Mander",
+    "High Steward of Highgarden (pre-Conquest)"
+  ]
+},
+{"name": "House Nymeros Martell of Sunspear",
+"greatHouse": true,
+"region": "Dorne",
+"coatOfArms": "Tenny, a sun in splendour gules transfixed by a spear bendwise Or",
+"words": "Unbowed, Unbent, Unbroken",
+"titles":  [
+    "Lord of the Sandship",
+    "Lord of Sunspear",
+    "Prince of Dorne"
+  ]
+},
+{"name": "House Mormont of Bear Island",
+"greatHouse": false,
+"region": "The North",
+"coatOfArms": "A wood vert, a bear sable",
+"words": "Here We Stand",
+"titles": [
+    "Lord of Bear Island"
+  ] 
+},
+{"name": "House Frey of Riverrun",
+"greatHouse": false,
+"region": "The Riverlands",
+"coatOfArms": "",
+"words": "",
+"titles": [
+    "Lord of Riverrun"
+  ] 
+},
+{"name": "House Bolton of the Dreadfort",
+"greatHouse": false,
+"region": "The North",
+"coatOfArms": "A red flayed man on pink de sang",
+"words": "Our Blades are Sharp",
+"titles": [
+    "Red King (Age of Heroes)",
+    "Lord of the Dreadfort",
+    "Warden of the North"
+  ] 
+},
+{"name": "House Karstark of Karhold",
+"greatHouse": false,
+"region": "The North",
+"coatOfArms": "Sable, a sunburst argent",
+"words": "The Sun of Winter",
+"titles": [
+    "Lord of Karhold"
+  ] 
+},
+{"name": "House Umber of the Last Hearth",
+"greatHouse": false,
+"region": "The North",
+"coatOfArms": "Gules, a roaring giant proper with shattered chains argent",
+"words": "",
+"titles": [
+    "Lord of the Last Hearth"
+  ] 
+},
+{"name": "House Baelish of Harrenhal",
+"greatHouse": false,
+"region": "The Riverlands",
+"coatOfArms": "A field of silver mockingbirds, on a green field(Vert, semé of mockingbirds argent)",
+"words": "",
+"titles": [
+    "Lord Paramount of the Trident",
+    "Lord of Harrenhal"
+  ] 
+},
+{"name": "House Tarly of Horn Hill",
+"greatHouse": false,
+"region": "The Reach",
+"coatOfArms": "Vert, a huntsman striding to dexter proper garbed gules",
+"words": "First in Battle",
+"titles": [
+    "Lord of Horn Hill"
+  ] 
+},
+{"name": "House Tully of Riverrun",
+"greatHouse": false,
+"region": "The Riverlands",
+"coatOfArms": "A leaping silver trout on a field of blue and mud red(Paly wavy azure and gules, a trout embowed argent)",
+"words": "Family, Duty, Honor",
+"titles": [
+    "Lord of Riverrun",
+    "Lord Paramount of the Trident"
+  ] 
+},
+{"name": "House Seaworth of Cape Wrath",
+"greatHouse": false,
+"region": "The Stormlands",
+"coatOfArms": "A black ship on a pale grey field, with a white onion on its sails(Cendrée, a ship sable, its sail charged with an onion argent)",
+"words": "",
+"titles": [""]
+},
+{"name": "House Reed of Greywater Watch",
+"greatHouse": false,
+"region": "The Neck",
+"coatOfArms": "Vert, a lizard-lion sable",
+"words": "",
+"titles": [""]
+},
+{"name": "House Clegane",
+"greatHouse": false,
+"region": "The Westerlands",
+"coatOfArms": "Three dogs on a yellow field(Or, three dogs courant in pale sable)",
+"words": "",
+"titles": [
+    "Ser"
+  ] 
+}
+]
