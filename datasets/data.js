11d6413a43ba0fe6ab455eed499f30394f481224
Jake <jakeadmire1@gmail.com>
2019-02-10 18:51:43 -0700

    Fix file system and export datasets

--- datasets/data.js
+++ datasets/data.js
@@ -1,3 +1,162 @@
+const companies = [
+  {
+    "name": "Atari SA",
+    "year-founded": 1972,
+    "year-defunct": "N/A",
+    "founders": [
+    "Nolan Bushnell", 
+    "Ted Dabney"
+    ],
+    "logo": { 
+      "name": "Fuji", 
+      "designer": "George Opperman", 
+      "link": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Atari_Official_2012_Logo.svg/440px-Atari_Official_2012_Logo.svg.png" 
+    },
+    "consoles": [
+    "Atari 2600", 
+    "Atari 800", 
+    "Atari 400", 
+    "Atari 5200", 
+    "Atari 7800", 
+    "Atari Lynx", 
+    "Atari Jaguar", 
+    "Atari VCS"
+    ]
+  },
+  {
+    "name": "Bandai Co., Ltd.",
+    "year-founded": 1950,
+    "year-defunct": "N/A",
+    "founders": ["Naoharu Yamashina"],
+    "logo": { 
+      "name": "Banzai mark", 
+      "designer": "N/A", 
+      "link": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/BANDAI.svg/282px-BANDAI.svg.png" 
+    },
+    "consoles": [
+    "Bandai Super Vision 8000", 
+    "Bandai Arcadia", 
+    "Playdia", 
+    "WonderSwan", 
+    "WonderSwan Color", 
+    "Swan Crystal"
+    ]
+  },
+  {
+    "name": "Coleco Industries, Inc.",
+    "year-founded": 1932,
+    "year-defunct": 1988,
+    "founders": ["Maurice Greenberg"],
+    "logo": { 
+      "name": "N/A", 
+      "designer": "N/A", 
+      "link": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Coleco_logo.svg/440px-Coleco_logo.svg.png" 
+    },
+    "consoles": [
+    "Telstar", 
+    "Electronic Quarterback", 
+    "ColecoVision", 
+    "Coleco Gemini", 
+    "Coleco Adam"
+    ]
+  },
+  {
+    "name": "Magnavox",
+    "year-founded": 1917,
+    "year-defunct": "N/A",
+    "founders": [
+    "Edwin Pridham", 
+    "Peter L. Jensen"
+    ],
+    "logo": { 
+      "name": "N/A", 
+      "designer": "N/A", 
+      "link": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Magnavox_Logo.svg/500px-Magnavox_Logo.svg.png" 
+    },
+    "consoles": [
+    "Magnavox Odyssey", 
+    "Odyssey2"
+    ]
+  },
+  {
+    "name": "Microsoft Corporation",
+    "year-founded": 1975,
+    "year-defunct": "N/A",
+    "founders": [
+    "Bill Gates", 
+    "Paul Allen"
+    ],
+    "logo": { 
+      "name": "N/A", 
+      "designer": "N/A", 
+      "link": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Microsoft_logo_%282012%29.svg/440px-Microsoft_logo_%282012%29.svg.png" 
+    },
+    "consoles": [
+    "Xbox", 
+    "Xbox 360", 
+    "Xbox One"
+    ]
+  },
+  {
+    "name": "Nintendo Co., Ltd.",
+    "year-founded": 1889,
+    "year-defunct": "N/A",
+    "founders": ["Fusajiro Yamauchi"],
+    "logo": { 
+      "name": "N/A", 
+      "designer": "N/A", 
+      "link": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Nintendo.svg/440px-Nintendo.svg.png" 
+    },
+    "consoles": [
+    "Color TV-Game", 
+    "Game & Watch", 
+    "Nintendo Entertainment System", 
+    "Game Boy", 
+    "Super Nintendo Entertainment System", 
+    "Virtual Boy", 
+    "Nintendo 64", 
+    "Game Boy Pocket", 
+    "Game Boy Color", 
+    "Game Boy Advance", 
+    "GameCube", 
+    "Game Boy Advance SP", 
+    "Nintendo DS", 
+    "Game Boy Micro", 
+    "Nintendo DS Lite", 
+    "Nintendo Wii", 
+    "Nintendo DSi", 
+    "Nintendo DSi XL", 
+    "Nintendo 3DS", 
+    "Nintendo Wii U", 
+    "Nintendo 2DS", 
+    "Wii Mini", 
+    "Nintendo Switch"
+    ]
+  },
+  {
+    "name": "Sony Corporation",
+    "year-founded": 1946,
+    "year-defunct": "N/A",
+    "founders": [
+    "Masaru Ibuka", 
+    "Akio Morita"
+    ],
+    "logo": { 
+      "name": "N/A", 
+      "designer": "N/A", 
+      "link": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Sony_logo.svg/440px-Sony_logo.svg.png" 
+    },
+    "consoles": [
+    "PlayStation", 
+    "PlayStation 2", 
+    "PlayStation 3", 
+    "PlayStation Portable", 
+    "PlayStation Vita", 
+    "PlayStation 4"
+    ]
+  }
+];
+
 const consoles = [
   {
     "name": "Atari 2600",
@@ -522,3 +681,8 @@ const consoles = [
     }
   }
 ];
+
+module.exports = {
+  companies,
+  consoles
+}
