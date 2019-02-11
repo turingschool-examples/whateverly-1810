9da4cb266cbc1b412b141bd9eb091f32f16d3a81
David Cisneros <Dcisneros101@gmail.com>
2019-02-11 08:52:41 -0700

    David's Dataset

---
+++ bts-data.js
@@ -0,0 +1,164 @@
+const btsInfo = {
+  "groupPhoto": "https://www.billboard.com/files/media/4jx8ehs-sjs2u-xm6sal-kxj-mc3-bts-y13dhcm-2018-bb5-hxaw2-zz0-billboard-bc-1548.jpg",
+  "albums": [
+    {
+      "title": "2 COOL 4 SKOOL",
+      "albumPhoto": "https://lh3.googleusercontent.com/0JVpVha4Hol9rnUsb4qEefe9KVyuxGyp3z4HCF0PUU1iUeYI44F_t5hpkZh90L07i0cdOmII=w300-rw",
+      "albumId": 1,
+      "releaseDate": "June 12, 2013"
+    },
+    {
+      "title": "O!RUL8,2?",
+      "albumPhoto": "https://lh3.googleusercontent.com/e5fjvDKc4nZzctZ0TKT5ncEWIw_mwF7zZlvCvFd43J1CF1RtjO2yUnTD4qkodA3dlshGZUIT=w300-rw",
+      "albumId": 2,
+      "releaseDate": "September 11, 2013"
+    },
+    {
+      "title": "Skool Luv Affair",
+      "albumPhoto": "https://lh3.googleusercontent.com/eNhdx6doKNSHz8xb1MjFmWaNPjXbl2gObDU9qzsWLIdD1opTP7KhOMMbsvbhRYJOkp7z4jA5fg=w300-rw",
+      "albumId": 3,
+      "releaseDate": "January 1, 2014"
+    },
+    {
+      "title": "Dark & Wild",
+      "albumPhoto": "https://lh3.googleusercontent.com/ytfbVVPa5Qd08mR0tR0Q6G59dQrOlOMJwJ6mzu_4ysi643R_EmoCC6WQcADfyUpIaNR5Y8d2MOk=w300-rw",
+      "albumId": 4,
+      "releaseDate": "January 1, 2014"
+    },
+    {
+      "title": "Wake Up",
+      "albumPhoto": "https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwja79rq4bLgAhWJ34MKHcvzBOkQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FWake_Up_(BTS_album)&psig=AOvVaw1JicHgA6mbgVv3hZWJFHuW&ust=1549943020643381",
+      "albumId": 5,
+      "japaneseRelease": true,
+      "releaseDate": "December 24, 2014"
+    },
+    {
+      "title": "The Most Beautiful Moment in Life Pt.1",
+      "albumPhoto": "https://lh3.googleusercontent.com/pFtMVnQ5TYAHMX7AbT5i0ui_mFdSJ6gFgPlJDIZj5iUcz81ldUEAV_a-CIXC12-cloxjcV5u-zo=w300-rw",
+      "albumId": 6,
+      "releaseDate": "April 29, 2015"
+    },
+    {
+      "title": "The Most Beautiful Moment in Life, Part 2",
+      "albumPhoto": "https://lh3.googleusercontent.com/2vJu-Y5t3w3l5fqe7GyJnzB2kC7Dc7wJ2WHZ9Ln2DHC98Focc2fLQqHegjRLQrgHdN3MR0de=w300-rw",
+      "albumId": 7,
+      "releaseDate": "November 30, 2015"
+    },
+    {
+      "title": "The Most Beautiful Moment in Life: Young Forever",
+      "albumPhoto": "https://lh3.googleusercontent.com/cDimvclpY65lhmbxpN5lxT1Uqb8o0OpraKpWy6Lu2Qed9zX0Ru-DTqX696duAezSWA6rRZs5-w=w300-rw",
+      "albumId": 8,
+      "releaseDate": "May 2, 2016"
+    },
+    {
+      "title": "Youth",
+      "albumPhoto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3grFgpXp_Eli-gbPcZeH1XjsZl-yw5pfM_KZxTeT_6SYyWh_7uvLh0S0",
+      "albumId": 9,
+      "japaneseRelease": true,
+      "releaseDate": "September 7, 2016"
+    },
+    {
+      "title": "Wings",
+      "albumPhoto": "https://lh3.googleusercontent.com/oV_5UUW7tu3Sr_VLpxAGFutM9XbwJzcsN-lqlIUQNXv2b2upzoHS1Ec7DBdH4gBnY_2m-DYnAQ=w300-rw",
+      "albumId": 10,
+      "releaseDate": "October 10, 2016"
+    },
+    {
+      "title": "The Best of BTS -Japan Edition-",
+      "albumPhoto": "https://vignette.wikia.nocookie.net/the-bangtan-boys/images/e/ef/The_Best_of_Bodan_Shonendan_Japan_Regular_Editon.jpg/revision/latest?cb=20170722235657",
+      "albumId": 11,
+      "japaneseRelease": true,
+      "releaseDate": "January 6, 2017"
+    },
+    {
+      "title": "Love Yourself: Her",
+      "albumPhoto": "https://lh3.googleusercontent.com/OvqI6dtEvsjrRo3ZxIaygfIw_ekGz6GeVYVnhXyM3JOcRH8UmiXRds_lO1-h7kpOypePKU4Q=w300-rw",
+      "albumId": 12,
+      "releaseDate": "September 18, 2017"
+    },
+    {
+      "title": "Face Yourself",
+      "albumPhoto": "https://lh3.googleusercontent.com/1eOK0be0qtaKEPMzAvjoGE5_zAcEsN7Vvi-HjINkbRF5MRlikVxL7g14NPMYOC7-gFRVaUwY=w300-rw",
+      "albumId": 13,
+      "japaneseRelease": true,
+      "releaseDate": "April 4, 2018"
+    },
+    {
+      "title": "Love Yourself: Tear",
+      "albumPhoto": "https://lh3.googleusercontent.com/JWrVRiRpkTLB0R_ut2OBr3EG4FKqYIYJgb3lsVNnb_Dmq7IEbG7FyWueC_3n7JTh8UrHHzDl4g=w300-rw",
+      "albumId": 14,
+      "releaseDate": " May 18, 2018"
+    },
+    {
+      "title": "Love Yourself: Answer",
+      "albumPhoto": "https://lh3.googleusercontent.com/Byhjxr-r367lZxzT-VktzVnTB9X0BPFPICiDhgeRlEXVR2t-BtXDEdz2l6KyPcw4cWClA9WErQ=w300-rw",
+      "albumId": 15,
+      "releaseDate": "August 24, 2018"
+    }
+  ],
+  "members": [
+    {
+      "stageName": "Suga",
+      "birthName": "Min Yoon-Gi",
+      "birthDay": "March 9, 1993",
+      "age": 25,
+      "position": "Lead Rapper",
+      "zodiacSign": "Pisces",
+      "image": "https://www.billboard.com/files/media/k3iu3n-jjmei-dme1-km4-0cdl-m2flg-o1mw-bts-j2u-2018-bb5-kk2e-billboard-1240.jpg"
+    },
+    {
+      "stageName": "Jungkook",
+      "birthName": "Jeon Jeongguk",
+      "birthDay": " September 1, 1997",
+      "age": 21,
+      "position": "Main Vocalist",
+      "zodiacSign": "Virgo",
+      "image": "https://www.billboard.com/files/styles/900_wide/public/media/v26dm3i-jd6ye2-m3u-x53h-z9-bts-pal12-1na-2018-bb5-billboard-sscj-1240.jpg"
+    },
+    {
+      "stageName": "V",
+      "birthName": "Kim Tae-hyung",
+      "birthDay": "December 30, 1995",
+      "age": 23,
+      "position": "Vocalist",
+      "zodiacSign": "Capricorn",
+      "image": "https://www.billboard.com/files/styles/900_wide/public/media/j82lam-m73n-ny3-ndba-y3-bts-91ns-nsi2-2018-bb5-billboard-ggfv0qa-1240.jpg"
+    },
+    {
+      "stageName": "Jin",
+      "birthName": "Kim Seok-jin",
+      "birthDay": "December 4, 1992",
+      "age": 26,
+      "position": "Visual",
+      "zodiacSign": "Sagittarius",
+      "image": "https://www.billboard.com/files/styles/900_wide/public/media/82ksk-j2m-0e3n-m2-smo81-ms3o-bts-16dj4o-2018-bb5-62jsl-billboard-1240.jpg"
+    },
+    {
+      "stageName": "Jimin",
+      "birthName": "Park Ji-min",
+      "birthDay": "October 13, 1995",
+      "age": 23,
+      "position": "Main Dancer",
+      "zodiacSign": "Libra",
+      "image": "https://www.billboard.com/files/styles/900_wide/public/media/lopdn6-ks39-mvnot4i-kdi33-skr4t-bts-29idcfmv-2018-bb5-fgt5-u-billboard-1240.jpg"
+    },
+    {
+      "stageName": "RM",
+      "birthName": "Kim Nam-Joon",
+      "birthDay": "September 12, 1994",
+      "age": 24,
+      "position": "Leader",
+      "zodiacSign": "Virgo",
+      "image": "https://www.billboard.com/files/styles/900_wide/public/media/7zi2m-2n2p-mj3-mss2bq-jm21-kwaik-bts-0sm2-2018-bb5-tuj7s1-billboard-1240.jpg"
+    },
+    {
+      "stageName": "J-Hope",
+      "birthName": "Jung Ho-seok",
+      "birthDay": "February 18, 1994",
+      "age": 24,
+      "position": "Lead Rapper,",
+      "zodiacSign": "Aquarius",
+      "image": "https://www.billboard.com/files/styles/900_wide/public/media/nc73n-ms092n-kjdk-cj2-ljfle2-bts-kci2-2018-bb5-jx3lk-s12fa3-billboard-1240.jpg"
+    }
+  ]
+}
